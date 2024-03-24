007d0ef160b471688f7e23ab99925e0d79128967
pgattic <pgattic@gmail.com>
2024-03-23 23:59:40 -0600

    Small syntax update, README update

--- README.md
+++ README.md
@@ -1,56 +1,88 @@
 
 # Cairn 🪨
 
-Stack-based (esoteric?) programming language
+Cairn is a stack-oriented programming language, and the interpreter code found here is my first-ever rust project!!!
+
+[Software Demo Video](https://youtu.be/4RxESsPvRfk)
 
 ## Description
+
+Cairn is meant to be a general-purpose stack-oriented programming language. All code is meant to be executed onto a single execution stack and evaluated into a single data stack.
+
+My main reason for creating this language was out of curiosity. While reading up on the inner workings of WebAssembly, I read that it uses a stack-based Virtual Machine code. This sounded very interesting to me. I looked at some example WASM assembly code, and realized that theoretically anything can be done in a stack-based language!
+
+So, while this project was mostly an exploratory venture for me, it also resulted in a very lightweight math-focused language that I can use for complex mathematical calculations that I need done quickly. I may use it as a scripting language for other personal projects as an alternative to Python or Bash, if it matures enough and gains general-purpose utility.
+
+### Basics
+
+- All code must be contained in a function.
+- The `$main` function is automatically invoked at runtime.
+- Whitespace (like newlines, extra spaces) is not significant. There is no concept of code blocks besides a function, so some kind of code nesting using brackets or indentation has no place in this language.
 
 ### Principles:
 
 - The only information allowed on the stack is numbers. Anything else attempting to go onto it must be evaluated to a number.
-- Avoid using the `_` (nop) procedure as often as possible.
+- Avoid using the `_` (drop) function as often as possible.
 
 Example:
 
 ```
+$fact
+  dup -- dup ?++:fact *         # Conditionals and recursion
 
-=fact dup 1 - ?fact:1 *
+$say_hi
+  "Hello, " print print "!" println
 
-=main "Hello World!" print _ 5 fact print _ 0 exit
+$main
+  10 5 + 7 * putln              # Print some math operations
 
+  10000 fact putln              # Use Factorial Function
+
+  "Hello world!" println        # Some stdoutput examples
+  "Preston" say_hi
+  "What is your name? " println
+  readln say_hi
 ```
 
 ## Built-in functions
 
 Note: `t` refers to the value at the top of the stack
 
-- [x] `put` (pops 1, pushes 0): prints the value at the top of the stack.
-- [x] `putc` (pops 1, pushes 0): prints the value at the top of the stack as an ASCII value.
-
-- `print` (pops `t+1`, pushes 1): prints to the standard output
-- `read` (pops 0, pushes 1): Reads one character from the standard input
+- `putln` (pops 1, pushes 0): prints the value at the top of the stack as an integer.
+- `println` (pops 1): prints as a string instead of an int
+- `readln` (pops 0, pushes 1): Reads a line from the standard input
 
 ### Math
 
-- [x] `+`: add (pops 2, pushes 1)
-- [x] `-`: subtract (pops 2, pushes 1) - suntracts value of top of stack from value underneath it
-- [x] `*`: multiply (pops 2, pushes 1) - multiplies the top two values
-- [x] `div`: divide (pops 2, pushes 1) - integer division
-- [ ] `=` (pops 2, pushes 1) - equality
+- `+`: add (pops 2, pushes 1)
+- `-`: subtract (pops 2, pushes 1) - suntracts value of top of stack from value underneath it
+- `*`: multiply (pops 2, pushes 1) - multiplies the top two values
+- `div`: divide (pops 2, pushes 1) - integer division
+- `=` (pops 2, pushes 1) - equality
+
+Other comparison operators have the same syntax that other languages use.
 
 ### Control Flow
 
-- [x] `_` (pops 1, pushes 0): no-op 
-- [x] `dup` (pops 0, pushes 1): - copies the value at the top of the stack
-- [x] `?[val1]:[val2]:...:[valN]` (pops 0, pushes 0): Match statement (described in further detail later)
-- [ ] `exit`: (pops 1, pushes 0) Halt program execution, return `t` as the status code
-- ``
+- `_` (pops 1, pushes 0): drop
+- `dup` (pops 0, pushes 1): - copies the value at the top of the stack
+- `over` (pops 0, pushes 1): - Copies the second-from-top value to the top of the stack
+- `?[val1]:[val2]:...:[valN]` (pops 0, pushes 0): Match statement (TODO: describe in further detail)
 
 ## Data Types
 
 - int: represented with their integer value
 - string: alias for a sequence of ints, followed by its length
 
+## Development Environment
 
+Cairn was developed using these tools:
 
+- Neovim
+- Rust compiler (didn't think I would ever have a favorite compiler, but here I am!)
+
+## Useful Resources
+
+- [The Rust Book](https://doc.rust-lang.org/book/)
+- [Let's get Rusty's video about Rust String types](https://youtu.be/CpvzeyzgQdw?si=b6_Z-e7RJNlGbvig)
 
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,16 @@ use num_bigint::Sign::Plus;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let target_file: String = if args.len() > 1 {args.get(1).expect("owie").to_string()} else {"test.crn".to_string()};
+    let target_file = if args.len() > 1 {
+        args.get(1).expect("owie")
+    } else {
+        "test.crn"
+    };
 
     let contents: String = fs::read_to_string(target_file).expect("Failed to read file");
 
     let code_lines = contents.split("\n");
-    let modified_lines  = code_lines // comments
+    let modified_lines = code_lines // code comments
         .map(|line| line.split("#").next().unwrap_or(""))
         .collect::<Vec<_>>()
         .join(" ");
@@ -32,6 +36,7 @@ fn main() {
         }
     }
 
+    //println!("{:?}", functions);
     _ = execute(functions);
 }
 
@@ -44,12 +49,19 @@ fn split_code(input: &String) -> Vec<String> {
         if i == '"' {
             in_str = !in_str;
         } else if !in_str {
-            if i == ' ' || i == '\n' {
+            if i == ' ' || i == '\n' { // Check for '\n' is redundant. Keeping it just in case.
                 if curr_str.len() > 0 {
                     result.push(curr_str);
                     curr_str = "".to_string();
                 }
                 continue;
+//            } else if "{}?:".contains(i) {
+//                if curr_str.len() > 0 {
+//                    result.push(curr_str);
+//                    curr_str = "".to_string();
+//                }
+//                result.push(i.to_string());
+//                continue;
             }
         }
         curr_str.push(i);
@@ -57,20 +69,50 @@ fn split_code(input: &String) -> Vec<String> {
     result
 }
 
+//fn find_bracket(code: &Vec<&str>, start_i: usize) -> usize {
+//    let mut bracket_bal = 1;
+//    let mut i = start_i;
+//    loop {
+//        if i > code.len() {
+//            return 0;
+//        }
+//        if code[i] == "}" {
+//            bracket_bal -= 1;
+//            if bracket_bal == 0 {
+//                return i
+//            }
+//        } else if code[i] == "{" {
+//            bracket_bal += 1;
+//        }
+//        i += 1;
+//    }
+//}
+//
+//fn end_of_opt(code: &Vec<&str>) -> usize {
+//    if code[0] == "{" {
+//        find_bracket(&code, 1)
+//    } else {
+//        0
+//    }
+//}
+
 fn execute(code: HashMap<&str, Vec<&str>>) -> Result<(), String> {
     let mut stack: Vec<BigInt> = Vec::new();
     let mut instructions: Vec<&str> = code["main"].iter().cloned().collect();
 
     while !instructions.is_empty() {
+        println!("{:?} {:?}", stack, instructions);
         let c_instr = instructions[0];
         instructions.remove(0);
-    //    println!("{}", c_instr);
         match c_instr.parse::<BigInt>() {
             Ok(val) => {
                 stack.push(val);
             }
             Err(_) => {
                 match c_instr { // Built-in procedures
+//                    "{" => {
+//                        instructions.remove(find_bracket(&instructions, 0));
+//                    }
                     "put" => {
                         if let Some(value) = stack.pop() {
                             print!("{}", value);
@@ -197,8 +239,8 @@ fn execute(code: HashMap<&str, Vec<&str>>) -> Result<(), String> {
                         stack.pop();
                     }
                     "dup" => {
-                        if let Some(&ref last_value) = stack.last() {
-                            stack.push(last_value.clone());
+                        if let Some(value) = stack.last() {
+                            stack.push(value.clone());
                         } else {
                             stack.push(BigInt::from(0));
                         }
@@ -211,14 +253,46 @@ fn execute(code: HashMap<&str, Vec<&str>>) -> Result<(), String> {
                             }
                         }
                     }
+                    "over" => {
+                        let val = if stack.len() > 2 {
+                            stack[stack.len()-2].clone()
+                        } else {
+                            BigInt::from(0)
+                        };
+                        stack.push(val);
+                    }
+//                    "?" => { // Match/If/Map command
+//                        if let Some(value) = stack.last() {
+//                            let mut curr_opt = BigInt::from(0);
+//                            loop {
+//                                println!("{:?} {:?}", stack, instructions);
+//                                if &curr_opt < value { // try to find the next option
+//                                    let e_o = end_of_opt(&instructions);
+//                                    if instructions[e_o + 1] == ":" {
+//                                        curr_opt += 1;
+//                                        instructions.remove(0);
+//                                        instructions.remove(0);
+//                                    } else {
+//                                        break;
+//                                    }
+//                                } else {
+//                                    while instructions[ end_of_opt(&instructions) + 1 ] == ":" {
+//                                        let e_o = end_of_opt(&instructions) + 1;
+//                                        instructions.drain(1..e_o+1);
+//                                    }
+//                                    break;
+//                                }
+//                            }
+//                        }
+//                    }
                     s if s.starts_with('?') => {
                         if c_instr.contains(':') {
                             let options: Vec<&str> = c_instr[1..].split(":").collect();
-                            if let Some(value) = stack.last() {
+                            if let Some(value) = stack.pop() {
                                 let index: usize = {
-                                    if *value < BigInt::from(0) {
+                                    if value < BigInt::from(0) {
                                         0
-                                    } else if *value >= BigInt::from(options.len()) {
+                                    } else if value >= BigInt::from(options.len()) {
                                         options.len()-1
                                     } else {
                                         value.to_usize().expect("YEET")
@@ -234,6 +308,12 @@ fn execute(code: HashMap<&str, Vec<&str>>) -> Result<(), String> {
                         let value = &s[1..s.len()-1];
                         stack.push(BigInt::from_bytes_be(Plus, value.as_bytes()));
                     }
+//                    s if s.starts_with('{') => { // nested code
+//                        let splits = split_code(&s[1..s.len()-1].to_string());
+//                        for i in splits.iter().rev() {
+//                            instructions.insert(0, &i.clone());
+//                        }
+//                    }
                     &_ => { // Code-defined procedures
                         if let Some(values) = code.get(c_instr) {
                             for value in values.iter().rev() {
